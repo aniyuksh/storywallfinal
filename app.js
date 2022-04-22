1f845ea8895b85ee553ba9d5b1e9c49e2ff7214f
aniyuksh <ozaaniyuksh@gmail.com>
2022-04-22 14:00:26 +0530

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
